3f42e87af1309e89d21aff3895351b2f9838d115
ColinVM <roberson.colin@gmail.com>
2023-03-14 21:32:41 -0600

    testing (unsuccessfully) with resource bundles - committing to ensure I do not lose data on the VM.

--- src/main/java/controller/LoginScreenController.java
+++ src/main/java/controller/LoginScreenController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.*;
 import roberson.qam2.Main;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import static DAO.UsersQuery.verifyUser;
@@ -21,6 +22,18 @@ public class LoginScreenController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // testing resource bundle and changing languages
+        try {
+            ResourceBundle rb = ResourceBundle.getBundle("language/Nat", Locale.getDefault());
+            if (Locale.getDefault().getLanguage().equals("fr")) {
+                usernameField.setPromptText(rb.getString("Username"));
+                passwordField.setPromptText(rb.getString("Password"));
+                submitLoginButton.setText(rb.getString("Submit"));
+                cancelLoginButton.setText(rb.getString("Cancel"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("Initialized!");
     }
 
