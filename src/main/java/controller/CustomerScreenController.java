b0a753827a7fa91af9500db32537956e0f18b9f8
Colin Roberson <roberson.colin@gmail.com>
2023-03-19 19:15:03 -0500

    Completed the customer screen table and columns, as well as added the base functionality (populate table).

--- src/main/java/controller/CustomerScreenController.java
+++ src/main/java/controller/CustomerScreenController.java
@@ -1,22 +1,39 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Customers;
 import roberson.qam2.Main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static DAO.CustomerQuery.getAllCustomers;
+
 public class CustomerScreenController implements Initializable {
 
+    @FXML
+    private  TableColumn<Customers, Integer> CustomerIdCol;
+    @FXML
+    private  TableColumn<Customers, String> CustomerNameCol;
+    @FXML
+    private  TableColumn<Customers, String> CustomerAddrCol;
+    @FXML
+    private  TableColumn<Customers, String> CustomerFirstLevelDivisionCol;
+    @FXML
+    private  TableColumn<Customers, String> CustomerCountryCol;
+    @FXML
+    private  TableColumn<Customers, String> CustomerPostalCodeCol;
+    @FXML
+    private  TableColumn<Customers, String> CustomerPhoneNumberCol;
+    @FXML
+    private  TableView<Customers> CustomerTable;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-    }
-
-    public void onActionExit(ActionEvent actionEvent) {
+    public void OnActionExit(ActionEvent actionEvent) {
         System.exit(0);
     }
 
@@ -29,5 +46,33 @@ public class CustomerScreenController implements Initializable {
 
     public void OnActionCustomers(ActionEvent actionEvent) {
         Main.switchStage(actionEvent, "/roberson/qam2/customer-screen.fxml");
+    }
+
+    public void OnActionAdd(ActionEvent actionEvent) {
+
+    }
+
+    public void OnActionUpdate(ActionEvent actionEvent) {
+
+    }
+
+    public void OnActionDelete(ActionEvent actionEvent) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<Customers> allCustomers = getAllCustomers();
+
+        CustomerTable.setItems(allCustomers);
+
+        CustomerIdCol.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+        CustomerNameCol.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        CustomerAddrCol.setCellValueFactory(new PropertyValueFactory<>("customerAddress"));
+        //CustomerFirstLevelDivisionCol.setCellValueFactory(new PropertyValueFactory<>("appointmentLocation"));
+        //CustomerCountryCol.setCellValueFactory(new PropertyValueFactory<>("contactId"));
+        CustomerPostalCodeCol.setCellValueFactory(new PropertyValueFactory<>("customerPostalCode"));
+        CustomerPhoneNumberCol.setCellValueFactory(new PropertyValueFactory<>("customerPhoneNumber"));
+
     }
 }
